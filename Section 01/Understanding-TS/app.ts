3c74c3f39f423cc85a0487af17790e07e5a2a5ef
Ahmed Al Naqbi <aqusoom@hotmail.com>
2024-04-18 22:35:42 +0400

    Typescript Object

--- Section 01/Understanding-TS/app.ts
+++ Section 01/Understanding-TS/app.ts
@@ -1,21 +1,9 @@
-function add(n1: number, n2: number, showRes: boolean, phase: string) {
-    // if ( typeof n1 == 'number' || typeof n2 == 'number' ) {
-    //     throw new Error('Incorrect input!');
-    // }
+const person: {
+    name: string;
+    age: number;
+} = {
+    name: 'Ahmed',
+    age: 37
+};
 
-    const res = n1 + n2;
-    if (showRes) {
-        console.log(phase + res);
-    } else {
-        return res;
-    }
-}
-
-let number1: number;
-number1 = 5;
-const number2 = 2.8;
-
-const printRes = true;
-let resPhase = 'Result is ';
-
-add(number1, number2, printRes, resPhase);
+console.log(person.name);
