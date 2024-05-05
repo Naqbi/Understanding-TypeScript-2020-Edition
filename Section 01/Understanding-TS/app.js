c336b312197e7a41144a98625f903a339fce2644
Ahmed Al Naqbi <aqusoom@hotmail.com>
2024-05-05 21:16:50 +0400

    Complete array and enum exerices.

--- Section 01/Understanding-TS/app.js
+++ Section 01/Understanding-TS/app.js
@@ -1,18 +1,25 @@
-function add(n1, n2, showRes, phase) {
-    // if ( typeof n1 == 'number' || typeof n2 == 'number' ) {
-    //     throw new Error('Incorrect input!');
-    // }
-    var res = n1 + n2;
-    if (showRes) {
-        console.log(phase + res);
-    }
-    else {
-        return res;
-    }
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
+var person = {
+    name: 'Ahmed',
+    age: 37,
+    hobbies: ['Cooking', 'Sports'],
+    role: Role.ADMIN
+};
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
 }
-var number1;
-number1 = 5;
-var number2 = 2.8;
-var printRes = true;
-var resPhase = 'Result is ';
-add(number1, number2, printRes, resPhase);
+if (person.role === Role.AUTHOR) {
+    console.log('is author');
+}
