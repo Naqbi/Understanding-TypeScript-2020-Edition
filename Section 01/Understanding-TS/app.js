22fb982d5ef9b0b0f00f13378aff58c09e14c90d
Ahmed Al Naqbi <aqusoom@hotmail.com>
2024-05-05 21:53:16 +0400

    Complete literal type section.

--- Section 01/Understanding-TS/app.js
+++ Section 01/Understanding-TS/app.js
@@ -1,4 +1,4 @@
-function combine(input1, input2) {
+function combine(input1, input2, resConv) {
     var res;
     if (typeof input1 == 'number' && typeof input2 == 'number') {
         res = input1 + input2;
@@ -6,9 +6,16 @@ function combine(input1, input2) {
     else {
         res = input1.toString() + input2.toString();
     }
-    return res;
+    if (resConv === 'as-number') {
+        return +res;
+    }
+    else {
+        return res.toString();
+    }
 }
-var combineAges = combine(30, 260);
+var combineAges = combine(30, 26, 'as-number');
 console.log(combineAges);
-var combinedNames = combine('Max', 'Ann');
+var combineStringAges = combine('30', '26', 'as-number');
+console.log(combineStringAges);
+var combinedNames = combine('Max', 'Ann', 'as-text');
 console.log(combinedNames);
--- Section 01/Understanding-TS/app.ts
+++ Section 01/Understanding-TS/app.ts
@@ -1,16 +1,23 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(input1: number | string, input2: number | string, resConv: 'as-number' | 'as-text') {
   let res;
   if (typeof input1 == 'number' && typeof input2 == 'number') {
     res = input1 + input2;
   } else {
     res = input1.toString() + input2.toString();
   }
-   
-  return res;
+
+  if (resConv === 'as-number') {
+    return +res; 
+  } else {
+    return res.toString();
+  }
 }
 
-const combineAges = combine(30, 260);
+const combineAges = combine(30, 26, 'as-number');
 console.log(combineAges);
 
-const combinedNames = combine('Max', 'Ann');
+const combineStringAges = combine('30', '26', 'as-number');
+console.log(combineStringAges);
+
+const combinedNames = combine('Max', 'Ann', 'as-text');
 console.log(combinedNames);
