e3deeee0827375b2327d3f64aca57010bbc44d01
Ahmed Al Naqbi <aqusoom@hotmail.com>
2024-05-14 13:27:09 +0400

    let and var section

--- Section 04/next-gen-01-starting-setup/src/app.ts
+++ Section 04/next-gen-01-starting-setup/src/app.ts
@@ -1,1 +1,17 @@
-// Code goes here!
+const userName = 'Ahmed';
+let age = 37;
+
+age = 36;
+var res;
+
+function add(a:number, b:number) {
+  res = a, b;
+
+  return res;  
+}
+
+if (age > 20) {
+  let isOld = true;
+}
+
+console.log(isOld);
