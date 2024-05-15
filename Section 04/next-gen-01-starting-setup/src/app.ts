1c8a7d286432ce4e8363034adc3503c588d0c08f
Ahmed Al Naqbi <aqusoom@hotmail.com>
2024-05-15 12:55:31 +0400

    Section 04: remove testing lines

--- Section 04/next-gen-01-starting-setup/src/app.ts
+++ Section 04/next-gen-01-starting-setup/src/app.ts
@@ -6,12 +6,11 @@ var res;
 
 function add(a:number, b:number) {
   res = a, b;
-
   return res;  
 }
 
-if (age > 20) {
-  let isOld = true;
-}
+// if (age > 20) {
+//   let isOld = true;
+// }
 
-console.log(isOld);
+// console.log(isOld);
