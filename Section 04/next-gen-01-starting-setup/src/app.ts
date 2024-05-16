6ae8239ff005f09ebea538a924c074e659181655
Ahmed Al Naqbi <aqusoom@hotmail.com>
2024-05-16 10:17:11 +0400

    Section 04: Arrow Functions - Part 1

--- Section 04/next-gen-01-starting-setup/src/app.ts
+++ Section 04/next-gen-01-starting-setup/src/app.ts
@@ -1,16 +1,22 @@
 const userName = 'Ahmed';
-let age = 37;
+// let age = 37;
 
-age = 36;
-var res;
+// age = 36;
+// var res;
 
-function add(a:number, b:number) {
-  res = a, b;
-  return res;  
-}
+// function add(a:number, b:number) {
+//   res = a, b;
+//   return res;  
+// }
 
 // if (age > 20) {
 //   let isOld = true;
 // }
 
 // console.log(isOld);
+
+const add = (a: number, b: number) => {
+  return a + b;
+} 
+
+console.log(add(2, 5));
