0788661b94b48dae5bdf7ceb574981f602cb6996
Ahmed Al Naqbi <aqusoom@hotmail.com>
2024-05-22 12:00:59 +0400

    Section 04: Complete whole section.

--- Section 04/next-gen-01-starting-setup/src/app.ts
+++ Section 04/next-gen-01-starting-setup/src/app.ts
@@ -15,6 +15,46 @@ const userName = 'Ahmed';
 
 // console.log(isOld);
 
-const add = (a: number, b: number) => a + b;
+//const add = (a: number, b: number = 1) => a + b;
 
-console.log(add(2, 5));
+//console.log(add(2, 5));
+
+const printOutput = (output: string | number) => console.log(output);
+
+const button = document.querySelector('button');
+
+if(button) {
+  button.addEventListener('click', event => console.log(event));
+}
+
+//printOutput(add(5));
+
+const hobbies = ['Sports', 'Cooking', 'Hiking', 'Reading', 'Programming'];
+const  activeHobbies = ['Hiking'];
+
+activeHobbies.push(...hobbies);
+
+const person = {
+  firstName: 'Ahmed',
+  age: 37
+}
+
+const copiedPerson = { ...person };
+
+const add = (...numbers: number[]) => {
+  numbers.reduce((curRes, curValue) => {
+  return curRes + curValue;  
+  }, 0);
+}
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
+
+const [h1, h2, ...remainingHobbies] = hobbies;
+
+console.log(hobbies);
+console.log(h1, h2, remainingHobbies);
+
+const { firstName: uName, age } = person;
+
+console.log(uName, age);
