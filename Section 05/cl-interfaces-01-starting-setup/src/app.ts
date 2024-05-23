ff38b992350a11cf5a0881bab0e84188d02f73ac
Ahmed Al Naqbi <aqusoom@hotmail.com>
2024-05-23 13:46:33 +0400

    Section 05: Complete classes sections.

--- Section 05/cl-interfaces-01-starting-setup/src/app.ts
+++ Section 05/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,11 +1,35 @@
 class Department {
-    name: string;
-    
-    constructor(n: string) {
-        this.name = n;
+    //private name: string;
+    //private readonly id: string;
+    private empolyees: string[] = [];
+    constructor(private readonly id: string, public name: string) {
+        //this.name = n;
+    }
+
+    describe(this: Department) {
+        console.log(`Department ${this.id}: ${this.name}`);
+    }
+
+    addEmloyee(employee: string) {
+        this.empolyees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.empolyees.length);
+        console.log(this.empolyees);
     }
 }
 
-const acounting = new Department('Accounting');
+const accounting = new Department('a1','Accounting');
 
-console.log(acounting);
+accounting.addEmloyee('Ahmed');
+accounting.addEmloyee('Ali');
+
+//accounting.empolyees[2] = 'Hassan';
+
+accounting.describe();
+accounting.printEmployeeInformation();
+
+// const copyAccounting = { name: 'Digital', describe: accounting.describe };
+
+// copyAccounting.describe(); // Department: undefined
