ef85bfa51dcb9883de63286a50ebecf3cdf04423
Ahmed Al Naqbi <aqusoom@hotmail.com>
2024-06-02 15:10:05 +0400

    Section 07: generics

---
+++ Section 07/generics-01-starting-setup/src/app.ts
@@ -0,0 +1,12 @@
+// const names:Array<string> = []; // string[]
+// //names[0].split(' ');
+
+// const promise = new Promise<number>((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve(10);
+//     })
+// });
+
+// promise.then(data => {
+//     //data.split(' ');
+// })
