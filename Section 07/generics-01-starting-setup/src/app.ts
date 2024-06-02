2417c19697a8ae54677d1b646f4c184f41186c6d
Ahmed Al Naqbi <aqusoom@hotmail.com>
2024-06-02 21:41:41 +0400

    Section 07: generic functions

--- Section 07/generics-01-starting-setup/src/app.ts
+++ Section 07/generics-01-starting-setup/src/app.ts
@@ -10,3 +10,16 @@
 // promise.then(data => {
 //     //data.split(' ');
 // })
+function merge<T, U>(objA: T, objB: U) {
+    //return Object.assign(objA, objB);
+    return { ...objA, ...objB };
+}
+
+const mergeObj = merge({ name: 'Ahmed', hobbies: ['Reading'] }, {age: '37'});
+
+const userAge = mergeObj.age;
+
+console.log(mergeObj); 
+console.log(userAge);
+
+console.log(mergeObj, userAge);
