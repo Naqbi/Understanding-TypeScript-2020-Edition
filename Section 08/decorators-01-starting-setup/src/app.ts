c8aec04cd9b2a5b3a753e6569186b228b8aedcb8
Ahmed Al Naqbi <aqusoom@hotmail.com>
2024-06-03 15:06:04 +0400

    Section 08: initial commit.

---
+++ Section 08/decorators-01-starting-setup/src/app.ts
@@ -0,0 +1,32 @@
+function Logger(logString: string) {
+    return function(constructor: Function) {
+        console.log(logString);
+        console.log(constructor);
+    }
+    
+}
+
+function WithTemplate(template: string, hookId: string) {
+    return function(constructor: any) {
+        const hookElement = document.getElementById(hookId)!;
+        const p = new constructor();
+        if(hookElement) {
+            hookElement.innerHTML = template;
+            hookElement.querySelector('h1')!.textContent = p.name;
+        }
+    }
+}
+
+//@Logger('LOGGING - PERSON')
+@WithTemplate('<h1>My Person Object</h1>', 'app')
+class Person {
+    name = 'Ahmed';
+
+    constructor() {
+        console.log('Creating person object...');        
+    }
+}
+
+const person = new Person();
+
+console.log(person);
