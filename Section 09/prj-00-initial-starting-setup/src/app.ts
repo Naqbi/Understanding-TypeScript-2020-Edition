a360390871448b1f509087297d60c9bcb12c8d9e
Ahmed Al Naqbi <aqusoom@hotmail.com>
2024-06-20 15:07:14 +0400

    Section 09: Creating & Using an /Autobind/ Decorator

--- Section 09/prj-00-initial-starting-setup/src/app.ts
+++ Section 09/prj-00-initial-starting-setup/src/app.ts
@@ -1,20 +1,56 @@
+function autobind(target: any, methodName: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor = {};
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleHtmlElement: HTMLInputElement;
+  descriptionHtmlElement: HTMLInputElement;
+  peopleHtmlElement: HTMLInputElement;
+
   constructor() {
     this.templateElement = document.getElementById(
       "project-input"
     )! as HTMLTemplateElement;
     this.hostElement = document.getElementById("app")! as HTMLDivElement;
 
-    const importedNode = document.importNode(this.templateElement.content, true /**/);
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true /**/
+    );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+
+    this.titleHtmlElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionHtmlElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleHtmlElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
 
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleHtmlElement.value);
+    console.log(this.descriptionHtmlElement.value);
+    console.log(this.peopleHtmlElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
+  }
+
   private attach() {
-    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
 }
 
