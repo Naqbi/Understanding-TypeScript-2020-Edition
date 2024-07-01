a7fe73d01bb459a1f5eb08abb288c169af007a97
Ahmed Al Naqbi <aqusoom@hotmail.com>
2024-07-01 10:32:28 +0400

    Section 09: Complete Creating a Re-Usable Validation Functionality.

--- Section 09/prj-00-initial-starting-setup/src/app.ts
+++ Section 09/prj-00-initial-starting-setup/src/app.ts
@@ -1,15 +1,48 @@
 // Validation
 interface Validatable {
   value: string | number;
-  required: boolean;
-  minLength: number;
-  maxLength: number;
-  min: number;
-  max: number;
-};
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
 
-function validate(validatableInput: Validatable) {
+function validate(validatableInput: Validatable): boolean {
   let isValid = true;
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+
+  if (
+    validatableInput.minLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length >= validatableInput.minLength;
+  }
+
+  if (
+    validatableInput.maxLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length < validatableInput.maxLength;
+  }
+  if (
+    validatableInput.min != null &&
+    typeof validatableInput.value === "number"
+  ) {
+    isValid = isValid && validatableInput.value >= validatableInput.min;
+  }
+
+  if (
+    validatableInput.max != null &&
+    typeof validatableInput.value === "number"
+  ) {
+    isValid = isValid && validatableInput.value < validatableInput.max;
+  }
+  return isValid;
 }
 
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
@@ -64,10 +97,27 @@ class ProjectInput {
     const enteredDescription = this.descriptionHtmlElement.value;
     const enteredPeople = this.peopleHtmlElement.value;
 
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
+    };
+
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    };
+
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+        max: 5
+    };
     if (
-      validate({ value: enteredTitle, required: true, minLength: 5 }) &&
-      Validate({ value: enteredDescription, required: true, minLength: 5 }) &&
-      validate({ value: enteredPeople, required: true, minLength: 5 })
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
     ) {
       alert("Invalid input, please try again!");
     } else {
